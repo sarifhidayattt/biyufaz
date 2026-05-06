0306a31bb6a0021c22cee25c6e393ee582b4b414
SHDAY <164343797+sarifhidayattt@users.noreply.github.com>
2026-05-06 16:58:01 +0700

    Add temporary folder creation for Laravel on Vercel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
-
-// Mengabaikan peringatan jika folder sudah ada
+// Membuat folder sementara untuk Laravel di server Vercel
 @mkdir('/tmp/storage/framework/views', 0755, true);
+@mkdir('/tmp/storage/framework/cache', 0755, true);
+@mkdir('/tmp/storage/framework/sessions', 0755, true);
 
 require __DIR__ . '/../public/index.php';
