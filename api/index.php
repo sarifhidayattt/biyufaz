0363ef4179e78f691e4fa10f6bc9db9038270b01
SHDAY <164343797+sarifhidayattt@users.noreply.github.com>
2026-05-06 16:47:27 +0700

    Ignore warning when creating cache directory
    Suppress warning when creating temporary cache folder.

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
-// Buat folder temporary untuk cache Laravel di Vercel
-mkdir('/tmp/storage/framework/views', 0755, true);
+
+// Mengabaikan peringatan jika folder sudah ada
+@mkdir('/tmp/storage/framework/views', 0755, true);
 
 require __DIR__ . '/../public/index.php';
