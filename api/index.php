c00dcf48601b1b30da397635641f90e9e88e1302
SHDAY <164343797+sarifhidayattt@users.noreply.github.com>
2026-05-06 17:03:42 +0700

    Setup temporary storage for Laravel on Vercel
    Added environment variables for temporary storage and created necessary directories for Laravel on Vercel.

--- api/index.php
+++ api/index.php
@@ -1,7 +1,12 @@
 <?php
-// Membuat folder sementara untuk Laravel di server Vercel
+
+// 1. Matikan pengecekan storage permanen
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+
+// 2. Buat folder view sementara agar Laravel tidak "panic"
 @mkdir('/tmp/storage/framework/views', 0755, true);
-@mkdir('/tmp/storage/framework/cache', 0755, true);
-@mkdir('/tmp/storage/framework/sessions', 0755, true);
 
 require __DIR__ . '/../public/index.php';
